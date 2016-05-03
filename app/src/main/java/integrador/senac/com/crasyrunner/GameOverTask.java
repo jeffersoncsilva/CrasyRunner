17a780aa32ef4d5b746748dd91d9576a5df9a08d
Jefferson C. Silva <jefferson.c.silva@outlook.com>
2016-05-03 19:17:55 -0300

    Botao de compartilhar com facebook funcionando perfeitamente.

--- app/src/main/java/integrador/senac/com/crasyrunner/GameOverTask.java
+++ app/src/main/java/integrador/senac/com/crasyrunner/GameOverTask.java
@@ -1,6 +1,7 @@
 package integrador.senac.com.crasyrunner;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -14,7 +15,15 @@ import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.Profile;
+import com.facebook.share.ShareApi;
+import com.facebook.share.Sharer;
 import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareDialog;
 
 /**
  * Created by Jefferson on 25/04/2016.
@@ -23,6 +32,55 @@ public class GameOverTask extends AsyncTask<Void, Void, Void> {
     private Activity act;
     long time;
     private PopupWindow pw;
+    private boolean canPresentShareDialog;
+    private PendingAction pendingAction = PendingAction.NONE;
+    private ShareDialog shareDialog;
+    private CallbackManager callbackManager;
+
+    private FacebookCallback<Sharer.Result> shareCallback = new FacebookCallback<Sharer.Result>() {
+        @Override
+        public void onCancel() {
+            Log.d("HelloFacebook", "Canceled");
+        }
+
+        @Override
+        public void onError(FacebookException error) {
+            Log.d("HelloFacebook", String.format("Error: %s", error.toString()));
+            //String title = getString(R.string.error);
+            //String alertMessage = error.getMessage();
+            //showResult(title, alertMessage);
+        }
+
+        @Override
+        public void onSuccess(Sharer.Result result) {
+            Log.d("HelloFacebook", "Success!");
+            if (result.getPostId() != null) {
+                Log.d("olafacebook", "houvesucesso.");
+                /*String title = getString(R.string.success);
+                String id = result.getPostId();
+                String alertMessage = getString(R.string.successfully_posted_post, id);
+                showResult(title, alertMessage);
+                */
+            }
+        }
+
+        private void showResult(String title, String alertMessage) {
+            Toast.makeText(act.getBaseContext(), "Ola, resultado recebido: " + alertMessage, Toast.LENGTH_LONG).show();
+            /*
+            new AlertDialog.Builder(HelloFacebookSampleActivity.this)
+                    .setTitle(title)
+                    .setMessage(alertMessage)
+                    .setPositiveButton(R.string.ok, null)
+                    .show();
+            */
+        }
+    };
+
+    private enum PendingAction {
+        NONE,
+        POST_PHOTO,
+        POST_STATUS_UPDATE
+    }
 
     public GameOverTask(Activity act){
         this.act = act;
@@ -43,8 +101,14 @@ public class GameOverTask extends AsyncTask<Void, Void, Void> {
     @Override
     protected void onPostExecute(Void result) {
         try {
+            canPresentShareDialog = ShareDialog.canShow(ShareLinkContent.class);
+            Log.d("presentsharedialog", "Valor: " + canPresentShareDialog);
             LayoutInflater inflater = (LayoutInflater) this.act.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             View view = inflater.inflate(R.layout.game_over, null, false);
+            callbackManager = CallbackManager.Factory.create();
+            shareDialog = new ShareDialog(act);
+            shareDialog.registerCallback(callbackManager, shareCallback);
+
             Button btInicio = (Button) view.findViewById(R.id.btnVoltarInicio);
             btInicio.setOnClickListener(new View.OnClickListener(){
                 @Override
@@ -54,17 +118,29 @@ public class GameOverTask extends AsyncTask<Void, Void, Void> {
                 }
             });
 
+
             Button btShareFb = (Button) view.findViewById(R.id.btShareFb);
             btShareFb.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Toast.makeText(act.getBaseContext(), "clique no btn de share", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(act.getBaseContext(), "clique no btn de share", Toast.LENGTH_SHORT).show();
 
-                ShareLinkContent content = new ShareLinkContent.Builder()
-                        .setContentUrl(Uri.parse("https://globo.com"))
+                Profile profile = Profile.getCurrentProfile();
+                ShareLinkContent linkContent = new ShareLinkContent.Builder()
+                        .setContentTitle("Hello Facebook")
+                        .setContentDescription("The 'Hello Facebook' sample  showcases simple Facebook integration")
+                        .setContentUrl(Uri.parse("http://developers.facebook.com/docs/android"))
                         .build();
-            }
-            });
+
+                if (canPresentShareDialog) {
+                    shareDialog.show(linkContent);
+                } else if (profile != null && hasPublishPermission()) {
+                    ShareApi.share(linkContent, shareCallback);
+                } else {
+                    pendingAction = PendingAction.POST_STATUS_UPDATE;
+                }
+
+            }});
 
             pw = new PopupWindow(view, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
             LinearLayout layout = new LinearLayout(act);
@@ -73,6 +149,11 @@ public class GameOverTask extends AsyncTask<Void, Void, Void> {
         catch(Exception e){
             Log.i("gameovertaskerro", "Erro: " + e.toString());
         }
+    }
+
+    private boolean hasPublishPermission() {
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        return accessToken != null && accessToken.getPermissions().contains("publish_actions");
     }
 
 }
