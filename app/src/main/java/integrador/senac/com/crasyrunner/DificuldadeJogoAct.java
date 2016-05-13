cf8f941b4ad3709fe89089c66a6068d54d90681d
Jefferson <jefferson.c.silva@outlook.com>
2016-05-13 14:00:43 -0300

    ajuste na hora de escolher dificuldades

--- app/src/main/java/integrador/senac/com/crasyrunner/DificuldadeJogoAct.java
+++ app/src/main/java/integrador/senac/com/crasyrunner/DificuldadeJogoAct.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by Jefferson on 02/05/2016.
@@ -16,23 +18,19 @@ public class DificuldadeJogoAct extends Activity {
         setContentView(R.layout.nivel_act);
     }
 
-    public void facil(View v){
+    public void escolheDificuldade(View v){
+        Button btn = (Button)v;
         Intent telaJogo = new Intent(DificuldadeJogoAct.this, TelaJogoActivity.class);
-        telaJogo.putExtra("nivel", 1);
-        startActivity(telaJogo);
-        finish();
-    }
 
-    public void medio(View v){
-        Intent telaJogo = new Intent(DificuldadeJogoAct.this, TelaJogoActivity.class);
-        telaJogo.putExtra("nivel", 2);
-        startActivity(telaJogo);
-        finish();
-    }
+        if(v.getId() == R.id.btnFacil){
+            telaJogo.putExtra("nivel", 1);
+        }
+        else if(v.getId() == R.id.btnMedio){
+            telaJogo.putExtra("nivel", 2);
+        }else if (v.getId() == R.id.btnDificio){
+            telaJogo.putExtra("nivel", 3);
+        }
 
-    public void dificio(View v){
-        Intent telaJogo = new Intent(DificuldadeJogoAct.this, TelaJogoActivity.class);
-        telaJogo.putExtra("nivel", 3);
         startActivity(telaJogo);
         finish();
     }
